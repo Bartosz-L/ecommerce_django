c21ee83887f245570bf2d96484d0ba5f8adf7282
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2020-01-16 16:07:06 +0100

    absolute url method in models

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'shop',
+    'shop.apps.ShopConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
