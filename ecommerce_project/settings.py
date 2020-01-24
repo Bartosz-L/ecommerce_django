792bc93a94d495689513ac75d6ece4cc5ca87949
Bartosz Ligęza <ligeza.bartosz@gmail.com>
2020-01-24 14:32:34 +0100

    local flavor to postal code

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'paypal.standard.ipn',
     'rosetta',
+    'localflavor',
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
     'orders.apps.OrdersConfig',
